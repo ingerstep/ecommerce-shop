1489b96e9e13bc7200e6c6df471f04bcb3f1fd30
Stepan <stepan.karelin.1996@mail.ru>
2024-06-10 15:55:32 +0700

    Created auth (email/google) with kinde.com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
@@ -97,9 +98,30 @@ export default function Home() {
                 alt="your image"
                 className="absolute w-40 lg:w-52 left-56 -top-20 select-none sm:block lg:hidden xl:block"
               />
-              <img src="/line.png" alt="line" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <img
+                src="/line.png"
+                alt="line"
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/**value proposition section */}
+      <section className="bg-slte-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
+            </h2>
+            <img src="/snake-2.png" alt="snake" className="w-24 order-0 lg:order-2" />
           </div>
         </MaxWidthWrapper>
       </section>
