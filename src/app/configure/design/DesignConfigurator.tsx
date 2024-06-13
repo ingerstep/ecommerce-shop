24ccebe33b03098769c6302a7eec9e5e95fd77a2
Stepan <stepan.karelin.1996@mail.ru>
2024-06-13 18:05:06 +0700

    Created scroll-area

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
 import NextImage from "next/image";
+import { Rnd } from "react-rnd";
+import { Radio, RadioGroup } from "@headlessui/react";
+import { useState } from "react";
+import { COLORS } from "@/validators/option-validator";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -12,6 +22,12 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number];
+  }>({
+    color: COLORS[0],
+  });
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -27,7 +43,89 @@ const DesignConfigurator = ({
               className="pointer-events-none z-50 select-none"
             />
           </AspectRatio>
+          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
+          <div
+            className={cn(
+              "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
+              `bg-${options.color.tw}`
+            )}
+          />
+
+          <Rnd
+            default={{
+              x: 100,
+              y: 105,
+              height: imageDimensions.height / 4,
+              width: imageDimensions.width / 4,
+            }}
+            lockAspectRatio
+            resizeHandleComponent={{
+              bottomRight: <HandleComponent />,
+              bottomLeft: <HandleComponent />,
+              topRight: <HandleComponent />,
+              topLeft: <HandleComponent />,
+            }}
+            className="relative w-full h-full pointer-events-auto"
+          >
+            <div className="relative w-full h-full">
+              <NextImage src={imageUrl} fill alt="your image" />
+            </div>
+          </Rnd>
         </div>
+      </div>
+
+      <div className="h-[37.5rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex fllex-col justify-between">
+              <RadioGroup
+                value={options.color}
+                onChange={(value) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: value,
+                  }));
+                }}
+              >
+                <Label>Colors: {options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <Radio
+                      key={color.label}
+                      value={color}
+                      className={({ focus, checked }) =>
+                        cn(
+                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                          {
+                            [`border-${color.tw}`]: focus || checked,
+                          }
+                        )
+                      }
+                    >
+                      <span
+                        className={cn(
+                          `bg-${color.tw}`,
+                          "h-8 w-8 rounded-full border border-black border-opacity-10"
+                        )}
+                      ></span>
+                    </Radio>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
---
+++ src/validators/option-validator.ts
@@ -0,0 +1,9 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-900 border-rose-900
+
+export const COLORS = [
+    { label: 'Black', value: 'black', tw: 'zinc-900' },
+    { label: 'Blue', value: 'blue', tw: 'blue-950' },
+    { label: 'Rose', value: 'rose', tw: 'rose-900' },
+] as const
